f59c19b6a52b83cce30cb0f2579518212c4cae4d
BrianSuarez <Brian.suarez0112@hotmail.com>
2025-04-14 18:03:49 -0500

    feat: :construction: Pagina "Creacion de personaje"
    feat: Pagina "Creacion de personaje"  Este commit inicia la creacion de La pagina de creacion de personajes:  - Se inicia creacion de HTML para pagina de "Creacion de personaje". - Se inicia construccion de CSS para adaptacion de pagina. - Se Agrega evento de escucha "click" para el boton de "Entrar" para validar usanrio en la API-Mockapi y devuelva la validacion de credenciales.

--- src/js/ui.js
+++ src/js/ui.js
@@ -66,4 +66,85 @@ export const formularioCreacionUsuario = `
     ¿Ya tienes una cuenta? <a id="iniciarSesion" href="#">Iniciar Sesion</a>
   </div>
 </section>
-`
+`;
+// HTMML Formulario de creacion de personaje
+export const formularioCreacionPersonaje = `
+<section class="section__crearPersonaje">
+    <h2>Crear Nuevo Personaje</h2>
+  
+    <!-- Datos básicos -->
+    <div class="div__nombre">
+      <label for="nombre">Nombre del personaje:</label>
+      <input type="text" id="cp-nombre" placeholder="">
+    </div>
+  
+    <div class="div__genero">
+      <label for="genero">Género:</label>
+      <select id="cp-genero">
+        <option value="">Seleccionar</option>
+        <option value="Masculino">Masculino</option>
+        <option value="Femenino">Femenino</option>
+        <option value="Otro">Otro</option>
+      </select>
+    </div>
+  
+    <!-- Datos a eleccion -->
+    <div class="div__raza">
+      <label for="raza">Raza:</label>
+      <select id="cp-raza">
+        <option value="">Cargando razas...</option>
+      </select>
+    </div>
+  
+    <div class="div__clase">
+      <label for="clase">Clase:</label>
+      <select id="cp-clase">
+        <option value="">Cargando clases...</option>
+      </select>
+    </div>
+  
+    <div class="div__armadura">
+      <label for="armadura">Armadura:</label>
+      <select id="cp-armadura">
+        <option value="">Cargando armaduras...</option>
+      </select>
+    </div>
+  
+    <div class="div__armas">
+      <label for="arma">Armas:</label>
+      <select id="cp-arma">
+        <option value="">Cargando armas...</option>
+      </select>
+    </div>
+  
+    <!-- Estadisticas -->
+    <div class="div__estadisticas">
+      <h3>Estadísticas</h3>
+      <label>Fuerza: <input type="number" id="fuerza" min="1" max="20"></label>
+      <label>Destreza: <input type="number" id="destreza" min="1" max="20"></label>
+      <label>Constitución: <input type="number" id="constitucion" min="1" max="20"></label>
+      <label>Inteligencia: <input type="number" id="inteligencia" min="1" max="20"></label>
+      <label>Sabiduría: <input type="number" id="sabiduria" min="1" max="20"></label>
+      <label>Carisma: <input type="number" id="carisma" min="1" max="20"></label>
+    </div>
+  
+    <!-- Datos habilidades -->
+    <div class="div__habilidades">
+      <label for="habilidades">Habilidades especiales:</label>
+      <select id="habilidades" multiple>
+        <option value="">Cargando habilidades...</option>
+      </select>
+    </div>
+  
+    <!-- Accesorios  -->
+    <div class="div__accesorios">
+      <label for="accesorios">Accesorios:</label>
+      <input type="text" id="accesorios" placeholder="Ej: Anillo de invisibilidad, Medalla élfica...">
+    </div>
+  
+    <!-- Boton Crear Personaje -->
+    <div class="div__botonCrear">
+      <button id="crearPersonaje">Crear personaje</button>
+    </div>
+  </section>
+`;
